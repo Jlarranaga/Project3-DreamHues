bb02f6aae9363ab1b6caca01fefd2e71196537a7
JacobL <Jacob_Larranaga@yahoo.com>
2024-01-25 15:48:21 -0700

    deploying

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ pip install --force-reinstall -U setuptools
 pip install django
 pip install dj_database_url
 pip install django-environ
+pip install psycopg2
+pip install boto3
+pip install whitenoise
+pip install gunicorn
 pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
