8bd3a77edee2b7ea6046ce6c5b69a1fb2a2d0026
JacobL <Jacob_Larranaga@yahoo.com>
2024-01-25 15:43:01 -0700

    deploying

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 pip install django
+pip install dj_database_url
+
 pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
