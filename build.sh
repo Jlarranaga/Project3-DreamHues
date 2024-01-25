ae16c37f86c3fe4dcbfcd63f09a8350ed8b889fe
JacobL <Jacob_Larranaga@yahoo.com>
2024-01-25 13:26:23 -0700

    deploying

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-# pip install --upgrade pip
-# pip install --force-reinstall -U setuptools
 pip install django
-pip install -r requirements.txt
+pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
